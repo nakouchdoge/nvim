9c57183f3faef7804f7f7ea82af6f5b1ae9082e2
nakouchdoge <nakouchdoge@gmail.com>
2024-05-10 17:28:56 -0700

    Added auto complete

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,4 +19,22 @@ return {
 		require("lazy-lsp").setup {}
 	  end
 	},
+
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	{
+		"williamboman/mason.nvim"
+	},
+	{
+		"hrsh7th/nvim-cmp"
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp"
+	},
+	{
+		"L3MON4D3/LuaSnip"
+	},
 }
