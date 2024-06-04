edbcad59c5c40bf00fc70c663c8e0af00dd79c21
nakouchdoge <nakouchdoge@gmail.com>
2024-06-04 14:30:02 -0500

    Removed uneeded plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -11,7 +11,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "bash", "php"},
+          ensure_installed = {"lua", "vim", "vimdoc", "query", "javascript", "html", "bash"},
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },
